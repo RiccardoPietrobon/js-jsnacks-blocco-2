16fdad42d2c697d79d2885d9e64f8da637d18397
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-01-27 18:48:55 +0100

    snack 1

---
+++ JSnack1/main.js
@@ -0,0 +1,18 @@
+//JSnack1
+//Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+let list_numbers = [];
+
+let my_number = 7;
+console.log(my_number);
+
+//let esito = true;
+
+if (my_number % 2 == 0) {
+    //esito = false;
+    console.log(my_number);
+}
+if (my_number % 2 != 0) {
+    //esito = true;
+    console.log(my_number + 1);
+}
+//console.log(esito);
